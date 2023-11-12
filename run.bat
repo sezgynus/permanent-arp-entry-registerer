fdddd56a3156e801bcc8da684a933f4dd30d2452
Sezgin AÇIKGÖZ <sezginacikgoz@mail.com>
2023-11-12 16:30:44 +0300

    *.py file name edit in run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-@echo off
-python ssh_commander.py 192.168.1.1 22 admin admin 192.168.1.2 00:d8:61:19:69:c8 root $F1r1@dl
+python permanent_arp_entry_registerer.py 192.168.1.1 22 admin admin 192.168.1.2 00:d8:61:19:69:c8 root $F1r1@dl
